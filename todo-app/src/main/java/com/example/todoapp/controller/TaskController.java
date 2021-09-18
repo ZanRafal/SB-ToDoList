6af06e8bb3d6d8241c45a808a4022ad61ae546c6
ZanRafal <01159225@pw.edu.pl>
2021-09-18 22:43:41 +0200

    Changed logic of entire code, Added RestController logic

--- todo-app/src/main/java/com/example/todoapp/controller/TaskController.java
+++ todo-app/src/main/java/com/example/todoapp/controller/TaskController.java
@@ -8,13 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RepositoryRestController
+@RestController
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskRepository.class);
--- todo-app/src/main/java/com/example/todoapp/model/TaskRepository.java
+++ todo-app/src/main/java/com/example/todoapp/model/TaskRepository.java
@@ -2,12 +2,12 @@ package com.example.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@RepositoryRestResource
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
     @Override
