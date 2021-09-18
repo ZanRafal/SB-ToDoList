4e68e8eca4783189e97e10b1c8f8cdaa020cd6ed
ZanRafal <01159225@pw.edu.pl>
2021-09-18 23:03:42 +0200

    Cleaned up imports

--- todo-app/src/main/java/com/example/todoapp/model/Task.java
+++ todo-app/src/main/java/com/example/todoapp/model/Task.java
@@ -2,7 +2,6 @@ package com.example.todoapp.model;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tasks")
