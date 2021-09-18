c8c8fe978021adc0b4aff2e881e876b265136107
ZanRafal <01159225@pw.edu.pl>
2021-09-18 20:43:42 +0200

    Changed mapping logic

--- todo-app/src/main/java/com/example/todoapp/controller/TaskController.java
+++ todo-app/src/main/java/com/example/todoapp/controller/TaskController.java
@@ -9,6 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RepositoryRestController
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
@@ -16,10 +19,8 @@ class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskRepository.class);
     @NonNull private final TaskRepository repository;
 
-//    public TaskController(final TaskRepository repository) {
-//        this.repository = repository;
-//    }
-
+    //@RequestMapping(method = RequestMethod.GET, path = "/tasks")
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
     ResponseEntity<?> readAllTasks() {
         logger.warn("Displaying all tasks!");
         return ResponseEntity.ok(repository.findAll());
