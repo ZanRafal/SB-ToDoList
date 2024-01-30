8dd63298dc80bb4a57c93fb089e4b830c4fe7453
Rafał Zan <01159225@pw.edu.pl>
2024-01-30 23:38:48 +0100

    Remove .idea file

---
+++ todo-app/src/main/java/com/example/todoapp/model/projection/GroupTaskWriteModel.java
@@ -0,0 +1,8 @@
+package com.example.todoapp.model.projection;
+
+import java.time.LocalDateTime;
+
+public class GroupTaskWriteModel {
+    private String description;
+    private LocalDateTime deadline;
+}
