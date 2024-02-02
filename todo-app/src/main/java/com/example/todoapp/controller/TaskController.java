20a93641d593e043d2d8641a32d18702947337dc
Rafał Zan <01159225@pw.edu.pl>
2024-02-02 12:50:36 +0100

    Add request mapping to make requests more sth..

--- todo-app/src/main/java/com/example/todoapp/controller/TaskController.java
+++ todo-app/src/main/java/com/example/todoapp/controller/TaskController.java
@@ -17,39 +17,45 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskRepository.class);
     @NonNull private final TaskRepository repository;
 
     //@RequestMapping(method = RequestMethod.GET, path = "/tasks")
-    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    @GetMapping(params = {"!sort", "!page", "!size"})
     ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Displaying all tasks!");
         return ResponseEntity.ok(repository.findAll());
     }
 
-    @GetMapping("/tasks")
+    @GetMapping()
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Displaying custom pageable!");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
-    @GetMapping("tasks/{id}")
+    @GetMapping("/search/done")
+    ResponseEntity<List<Task>> readDoneTasks(@RequestParam(defaultValue = "true") boolean state) {
+        return ResponseEntity.ok(repository.findByDone(state));
+    }
+
+    @GetMapping("/{id}")
     ResponseEntity<Task> readTask(@PathVariable int id) {
         return repository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/tasks")
+    @PostMapping()
     ResponseEntity<Task> createTask(@Valid @RequestBody Task toCreate) {
         logger.info("Successfully added new task");
         Task newTask = repository.save(toCreate);
         return ResponseEntity.created(URI.create("/" + newTask.getId())).body(newTask);
     }
 
-    @PutMapping("/tasks/{id}")
+    @PutMapping("/{id}")
     ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) {
         if(!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
@@ -65,7 +71,7 @@ class TaskController {
     @Transactional// Spring creates a proxy that implements the same interface(s) as the class you're annotating, metoda musi być w beanie i publiczna
     //po wywołaniu metody task się zmieni i zostanie to zacommitowane do bazy danych
     //transakcja sie nie wykona gdy poleci wyjątek
-    @PatchMapping("/tasks/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<?> toggleTask(@PathVariable int id) {
         if(!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
